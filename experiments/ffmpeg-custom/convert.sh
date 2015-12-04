c48a13c336d858bc82341f91fbc252eb33ba867a
HU Pili <hpl1989@gmail.com>
2015-12-05 01:06:37 +0800

    Resize images. This version achieves:
    * The final output has correct images
    * The audio is left shifted
    
    resize images. This everion:

--- experiments/ffmpeg-custom/convert.sh
+++ experiments/ffmpeg-custom/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ffmpeg -f image2 -r 1/1 -i image01.png -vcodec mpeg4 -y movie1.mp4
-ffmpeg -f image2 -r 1/5 -i image02.png -vcodec mpeg4 -y movie2.mp4
-ffmpeg -f image2 -r 1/2 -i image03.png -vcodec mpeg4 -y movie3.mp4
+ffmpeg -f image2 -r 0.9 -i image01.png -vcodec mpeg4 -y movie1.mp4
+ffmpeg -f image2 -r 0.23 -i image02.png -vcodec mpeg4 -y movie2.mp4
+ffmpeg -f image2 -r 0.5 -i image03.png -vcodec mpeg4 -y movie3.mp4
 
 ffmpeg -f concat -i playlist.txt -c copy output.mp4
 
