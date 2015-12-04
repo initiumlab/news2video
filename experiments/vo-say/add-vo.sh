be758de6337ee0d74e6e32cdb0422e333d741cba
HU Pili <hpl1989@gmail.com>
2015-12-04 23:29:11 +0800

    change VO to mp4 format

--- experiments/vo-say/add-vo.sh
+++ experiments/vo-say/add-vo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-say --input-file=vo.lrc --output-file=tts.m4a --voice=daniel --rate=150 --progress --file-format=m4af
+say --input-file=vo.lrc --output-file=tts.mp4 --voice=daniel --rate=150 --progress --file-format=mp4f
 
-ffmpeg -i output.mp4 -i tts.m4a -map 0:0 -map 1 -vcodec copy -acodec copy -y output-with-vo.mp4
+ffmpeg -i output.mp4 -i tts.mp4 -map 0:0 -map 1 -vcodec copy -acodec copy -y output-with-vo.mp4
 
 
 
