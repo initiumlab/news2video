304e7a257ba0fa54f547b209eb67b5e0338dbaa5
HU Pili <hpl1989@gmail.com>
2015-12-05 01:31:38 +0800

    fix my test experiments

--- experiments/vo-say/add-vo.sh
+++ experiments/vo-say/add-vo.sh
@@ -3,7 +3,7 @@
 
 say --input-file=vo.lrc --output-file=tts.mp4 --voice=daniel --rate=150 --progress --file-format=mp4f
 
-ffmpeg -i output.mp4 -i tts.mp4 -map 0:0 -map 1 -vcodec copy -acodec copy -y output-with-vo.mp4
+ffmpeg -i output.mp4 -i tts.mp4 -vcodec copy -acodec copy -y output-with-vo.mp4
 
 
 
